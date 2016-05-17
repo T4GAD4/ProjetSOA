9869f791ff3b065de32f456810279c352c39c7ca
T4GAD4 <T4GAD4@DESKTOP-8M4FHAB>
2016-05-17 17:20:32 +0200

    Correction de la fonction Post (Marche)

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
 <?php
-
+//include('services/produits.class.php');
+//include('services/database.class.php');
 // Autoload des class
 
 function __autoload($name) {
@@ -7,8 +8,9 @@ function __autoload($name) {
     if (file_exists($file))
     {
         include $file;
-    } 
-    throw new Exception("Impossible de charger la classe $name.");
+    }else{
+        throw new Exception("Impossible de charger la classe $name.");
+    }
 }
 
 // On récupére la méthode, la classe et les paramétres
@@ -16,29 +18,53 @@ $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['REQUEST_URI'],'/'));
 array_shift($request);
 
+if($method == "POST" || $method == "PUT"){
+    // On récupére les paramétres
+    $class = explode('?',$request[0])[0];
+    $params = explode('?',$request[0]);
+    if(isset($params[1])){
+        $params = $params[1];
+    }
+    $params = explode('&',$params);
+
+    //On va les récupérer dans un tableau
+    $parametres = array();
+    foreach($params as $param){
+        $parametres[explode('=', $param)[0]] = (isset(explode('=', $param)[1])) ? explode('=', $param)[1] : null;
+    }
+}else{
+    $class = $request[0];
+    if(isset($request[1])){
+        $parametres = $request[1];
+    }else{
+        $parametres = null;
+    }
+}
+
+
 if(sizeof($request) == 0){
     echo "Vous êtes sur l'accueil !";
     die();
 }
 
 // On appel la méthode de redirection
-$class = ucfirst($request[0]);
-
 try {
     $service = new $class();
 } catch (Exception $e) {
     die($e->getMessage());
 }
-$requete = $method. $request[0];
+$class = ucfirst($class);
+$requete = $method. $class;
 
 switch($method){
-    case 'GET' : $requete =  $method. $request[0]; if(!empty($request[1])){$params = $request[1];}break;
-    case 'POST' : $params = $_REQUEST; break;
-    case 'PUT' : $params = $_REQUEST; break;
-    case 'DELETE' : $requete = $method. $request[0];if(sizeof($request) == 2){$params = $request[1];}break;
+    case 'GET' : if(!empty($request[1])){$params = $request[1];}break;
+    case 'POST' : $params = $parametres; break;
+    case 'PUT' : $params = $parametres; break;
+    case 'DELETE' : if(sizeof($request) == 2){$params = $request[1];}break;
 }
-if(isset($params)){
-    $result = $service->$requete($params);
+
+if(isset($parametres)){
+    $result = $service->$requete($parametres);
 }else{
     $result = $service->$requete();
 }
